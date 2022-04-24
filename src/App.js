09212892f9676b9e43c173d47ad2632c29656926
Chapobulli <94289941+Chapobulli@users.noreply.github.com>
2022-04-24 11:08:19 +0200

    button add

--- src/App.js
+++ src/App.js
@@ -205,6 +205,16 @@ function App() {
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         </a>
         <s.SpacerSmall />
+        <StyledButton
+                onClick={(e) => {
+                  window.open("https://agoracro.com/collection/smartmartians", "_blank");
+                }}
+                style={{
+                  margin: "50px",
+                }}
+              >
+                Agoracro
+              </StyledButton>
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
