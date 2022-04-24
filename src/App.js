a6fd09edfd7109a2bd84e2dde3e3a56bb30bd721
Chapobulli <94289941+Chapobulli@users.noreply.github.com>
2022-04-24 11:37:18 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -317,7 +317,7 @@ function App() {
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
+                  Mint Your {CONFIG.SYMBOL} now. costs only {CONFIG.DISPLAY_COST}{" "}
                   {CONFIG.NETWORK.SYMBOL}.
                 </s.TextTitle>
                 <s.SpacerXSmall />
